8f52c73e94037dc77bd746dca83739588ba32694
djappa rayen <rayen.aloui733@gmail.com>
2022-01-10 23:28:40 +0100

    first commit

---
+++ src/main/java/com/example/jeudecartes/JeuDeCartesApplication.java
@@ -0,0 +1,13 @@
+package com.example.jeudecartes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeuDeCartesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeuDeCartesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jeudecartes/JeuDeCartesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jeudecartes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeuDeCartesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
