58776904433ef2707000f14483dfbf89dfec2a37
djappa rayen <rayen.aloui733@gmail.com>
2022-01-12 03:25:32 +0100

    implementation of unit tests

---
+++ src/test/java/com/example/jeudecartes/Resources/CardResourceTest.java
@@ -0,0 +1,53 @@
+package com.example.jeudecartes.Resources;
+
+import com.example.jeudecartes.Models.Card;
+import com.example.jeudecartes.Models.Order;
+import com.example.jeudecartes.Services.CardService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class CardResourceTest  {
+
+    @MockBean
+    CardService cardService;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+    }
+
+    @Test
+    void dealCards() throws Exception {
+        List<Card> cards = Arrays.asList(new Card("Pique","As"),new Card("Carreaux","6"));
+        Mockito.when(cardService.CreateDeck()).thenReturn(cards);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/cards/deal"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].color", Matchers.is("Pique")));
+    }
+}
